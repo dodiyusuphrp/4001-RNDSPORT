e7fabe57d1702bedec43ccb8fc0db9394cd9a463
Rafly Zulfi Robbiansyah <raflyzulfi1@gmail.com>
2019-03-20 15:43:09 +0700

    Menyelesaikan fitur registrasi. closes #3

--- app/src/main/java/com/petrus/asus/sportrnd/MainActivity.java
+++ app/src/main/java/com/petrus/asus/sportrnd/MainActivity.java
@@ -1,23 +1,42 @@
 package com.petrus.asus.sportrnd;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+///////////////////////
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+///////////////////////
+
 public class MainActivity extends AppCompatActivity {
     private Button btnLogin;
     private EditText username;
     private EditText password;
+
+    ///////////////////////
+    private FirebaseAuth firebaseAuth;
+    ///////////////////////
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         username = findViewById(R.id.username);
         password = findViewById(R.id.password);
+
+        ///////////////////////
+        firebaseAuth = FirebaseAuth.getInstance();
+        ///////////////////////
 
         btnLogin = findViewById(R.id.btnlogin);
         btnLogin.setOnClickListener(new View.OnClickListener() {
@@ -26,16 +45,23 @@ public class MainActivity extends AppCompatActivity {
                 String namaNya = username.getText().toString();
                 String passwordNya = password.getText().toString();
 
-                if(namaNya.equals("rnd")&& passwordNya.equals("rnd")){
-                    Intent next = new Intent(MainActivity.this, activity_halamanutama.class);
-                    startActivity(next);
-                    finish();
-                } else if(namaNya.matches("")|| passwordNya.matches("")){
-                    Toast.makeText(getApplication(),"Usernmae atau password tidak boleh kosong", Toast.LENGTH_LONG).show();
-                }
-                else {
-                    Toast.makeText(getApplication(), "Gagal Login, Username atau password Salah", Toast.LENGTH_LONG).show();
-                }
+                Log.d("USERNAME", namaNya);
+                Log.d("PASSWORD", passwordNya);
+
+                ///////////////////////
+                login(namaNya, passwordNya);
+                ///////////////////////
+
+//                if(namaNya.equals("rnd")&& passwordNya.equals("rnd")){
+//                    Intent next = new Intent(MainActivity.this, activity_halamanutama.class);
+//                    startActivity(next);
+//                    finish();
+//                } else if(namaNya.matches("")|| passwordNya.matches("")){
+//                    Toast.makeText(getApplication(),"Usernmae atau password tidak boleh kosong", Toast.LENGTH_LONG).show();
+//                }
+//                else {
+//                    Toast.makeText(getApplication(), "Gagal Login, Username atau password Salah", Toast.LENGTH_LONG).show();
+//                }
             }
         });
     }
@@ -44,4 +70,34 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(MainActivity.this, activity_daftar.class);
         startActivity(intent);
     }
+    ///////////////////////
+    private void login(String email, String password){
+        final ProgressDialog progressDialog = new ProgressDialog(MainActivity.this);
+        progressDialog.setMessage("Login...");
+        progressDialog.show();
+
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        progressDialog.dismiss();
+                        task.addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                e.printStackTrace();
+                            }
+                        });
+                        if (task.isSuccessful()) {
+                            Intent intent = new Intent(MainActivity.this, activity_halamanutama.class);
+                            startActivity(intent);
+                            finish();
+                        } else {
+//                            task.getResult().toString();
+                            Toast.makeText(MainActivity.this, "Gagal login", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    ///////////////////////
 }
--- app/src/main/java/com/petrus/asus/sportrnd/activity_daftar.java
+++ app/src/main/java/com/petrus/asus/sportrnd/activity_daftar.java
@@ -1,14 +1,30 @@
 package com.petrus.asus.sportrnd;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+///////////////////////
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+///////////////////////
+
 
 public class activity_daftar extends AppCompatActivity {
     private EditText username, password, namalengkap, phone, mail;
     private Button btnsave;
+
+
+    ///////////////////////
+    private FirebaseAuth firebaseAuth;
+    ///////////////////////
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,10 +45,42 @@ public class activity_daftar extends AppCompatActivity {
                 String addphone = phone.getText().toString();
                 String addmail = mail.getText().toString();
 
+                /////////////
+                registerUser(addmail, addpassword);
+                /////////////
             }
         });
 
+
+        firebaseAuth = FirebaseAuth.getInstance();
     }
 
+    ////////////////////////////////////////////////////////
+
+    private void registerUser(String email, String password){
+
+        final ProgressDialog progressDialog = new ProgressDialog(activity_daftar.this);
+        progressDialog.setMessage("Mendaftar...");
+        progressDialog.show();
+
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        progressDialog.dismiss();
+
+                        if(task.isSuccessful()){
+                            Toast.makeText(activity_daftar.this, "Registrasi berhasil", Toast.LENGTH_SHORT).show();
+                            finish();
+                        }else{
+                            Toast.makeText(activity_daftar.this, "Registrasi gagal", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+
+    }
+    ////////////////////////////////////////////////////////
 }
 
