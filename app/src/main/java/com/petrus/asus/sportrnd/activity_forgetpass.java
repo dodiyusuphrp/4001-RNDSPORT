e0536c0cbe0162cf7e254eb912a3b85157d67102
asus <revosaifuddin@gmail.com>
2019-04-04 14:22:20 +0700

    forget password selesai. closed#11

--- app/src/main/java/com/petrus/asus/sportrnd/activity_forgetpass.java
+++ app/src/main/java/com/petrus/asus/sportrnd/activity_forgetpass.java
@@ -1,8 +1,8 @@
 package com.petrus.asus.sportrnd;
 
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
@@ -71,4 +71,4 @@ public class activity_forgetpass extends AppCompatActivity {
     }
 
 }
-///////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////
