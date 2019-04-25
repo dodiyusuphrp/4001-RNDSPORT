ba460f05f9f745c4a4882e60f1aa672c3378906c
Your Name <you@example.com>
2019-04-25 17:38:54 +0700

    initial commit to destyharday

--- app/src/main/java/com/petrus/asus/sportrnd/PesanActivity.java
+++ app/src/main/java/com/petrus/asus/sportrnd/PesanActivity.java
@@ -3,6 +3,7 @@ package com.petrus.asus.sportrnd;
 import android.annotation.SuppressLint;
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -17,27 +18,56 @@ import java.util.Calendar;
 public class PesanActivity extends AppCompatActivity {
     private EditText edtnama, edtuangmuka, edtharga;
     private Button btnproses;
-    private TextView txtnama;
-    TextView pilihtanggal;
-    TextView pilihwaktu;
-    private TextView txtuangmuka;
+    private Button btnkonfirmasi;
+    private Button btnout;
+    private TextView txtpelanggan;
+    private TextView tanggal;
+    private TextView waktu;
     private TextView txtharga;
+    private TextView txtuangbayar;
+    private TextView txtname;
+    private TextView txttanggal;
+    private TextView txtwaktu;
+    private TextView txttotal;
+    private TextView txtkembalian;
     DatePickerDialog dpd;
     TimePickerDialog tpd;
+    private static final String PREFS_NAME = "prefs";
+    private static final String PREF_DARK_THEME = "dark_theme";
+    private static final String PREF_FONT_LARGE = "font_large" ;
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        SharedPreferences preferences = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+        boolean theme = preferences.getBoolean(PREF_DARK_THEME, false);
+        boolean font = preferences.getBoolean(PREF_FONT_LARGE, false);
+        if (theme && font) {
+            setTheme(R.style.AppTheme_Dark_FontLarge);
+        } else if (theme) {
+            setTheme(R.style.AppTheme_Dark_FontNormal);
+        } else if (font) {
+            setTheme(R.style.AppTheme_FontLarge);
+        }
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pesan);
 
-        btnproses = (Button) findViewById(R.id.tombol1);
-        edtnama = (EditText) findViewById(R.id.edtnama);
-        edtuangmuka = (EditText) findViewById(R.id.uangmuka);
+
+        edtnama = (EditText) findViewById(R.id.namapelanggan);
+        edtuangmuka = (EditText) findViewById(R.id.uangbayar);
         edtharga = (EditText) findViewById(R.id.harga);
-        txtnama = (TextView) findViewById(R.id.txtnama);
-        pilihtanggal = (TextView) findViewById(R.id.pilihtanggal);
-        pilihwaktu = (TextView) findViewById(R.id.pilihwaktu);
+        btnproses = (Button) findViewById(R.id.tombol1);
+        btnkonfirmasi = (Button) findViewById(R.id.tombol2);
+        btnout = (Button) findViewById(R.id.tombol3);
+        txtpelanggan = (TextView) findViewById(R.id.namapelanggan);
+        tanggal = (TextView) findViewById(R.id.pilihtanggal);
+        waktu = (TextView) findViewById(R.id.pilihwaktu);
         txtharga = (TextView) findViewById(R.id.harga);
-        txtuangmuka = (TextView) findViewById(R.id.uangmuka);
+        txtuangbayar = (TextView) findViewById(R.id.uangbayar);
+        txtname = (TextView) findViewById(R.id.customer);
+        txttanggal = (TextView) findViewById(R.id.date);
+        txtwaktu = (TextView) findViewById(R.id.time);
+        txttotal = (TextView) findViewById(R.id.total);
+        txtkembalian = (TextView) findViewById(R.id.sisa);
 
 
         //memberikan action pada tombol proses
@@ -46,18 +76,35 @@ public class PesanActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 String namapelanggan = edtnama.getText().toString().trim();
-                String uangmuka = edtuangmuka.getText().toString().trim();
-                String tanggal= pilihtanggal.getText().toString().trim();
-                String waktu= pilihwaktu.getText().toString().trim();
+                String pilihtanggal = tanggal.getText().toString().trim();
+                String pilihwaktu = waktu.getText().toString().trim();
                 String harga = edtharga.getText().toString().trim();
+                String uangbayar = txtuangbayar.getText().toString().trim();
 
-                double um = Double.parseDouble(uangmuka);
                 double h = Double.parseDouble(harga);
-                double total = (h - um);
+                double ub = Double.parseDouble(uangbayar);
+                double total = (h - ub);
+                txtname.setText("Nama Pelanggan : " );
+                txttanggal.setText("Tanggal : " );
+                txtwaktu.setText("Jam : ");
+                txtkembalian.setText("Sisa Kembalian : " + total);
+            }
+        });
+
+        //memberikan action pada tombol konfirmasi
+        btnkonfirmasi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
             }
         });
 
+        // memberikan action pada tombol keluar
+        btnout.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                moveTaskToBack(true);
+            }
+        });
     }
 
     public void setTime(final View view) {
@@ -65,7 +112,7 @@ public class PesanActivity extends AppCompatActivity {
             @Override
             public void onTimeSet(TimePicker timePicker, int i, int i1) {
                 if (view.getId() == R.id.pilihwaktu) {
-                    pilihwaktu.setText(i + ":" + i1);
+                    waktu.setText(i + ":" + i1);
                 }
                 tpd.dismiss();
             }
@@ -81,7 +128,7 @@ public class PesanActivity extends AppCompatActivity {
             @Override
             public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
                 if (view.getId() == R.id.pilihtanggal) {
-                    pilihtanggal.setText(i2 + "/" + (i1 + 1) + "/" + i);
+                    tanggal.setText(i2 + "/" + (i1 + 1) + "/" + i);
                 }
                 dpd.dismiss();
             }
